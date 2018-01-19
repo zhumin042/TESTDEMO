7a278e3f3d44d5ec1cce4bfa408aecadc3f82ec0
zhumin <zhumin042@163.com>
2018-01-19 22:42:18 +0800

    unit test use  karma

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        './tests/unit/**/*.spec.js'
+        './tests/unit/**/*.js'
     ],
 
 
@@ -50,7 +50,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
--- unit/index.spec.js
+++
@@ -1,5 +0,0 @@
-describe('测试XX编写部分', function(){
-	it('函数的基本加法结果测试',function(){
-		expect(add(1)).toBe(2)
-	});
-});
